76dafd9e56b1f0f571a35d846489a18293f2462d
jhorsch <jhorsch@Jeffs-MacBook-Air.local>
2013-10-22 19:43:14 -0500

    finished

--- app/controllers/airports_controller.rb
+++ app/controllers/airports_controller.rb
@@ -11,7 +11,14 @@ end
 
 
 def my_airports
-  return ['ORD', 'LAX', 'BOS', 'MDW', 'PDX', 'AUS']
+  return [
+    { id: '1', code: 'ORD', city: 'Chicago'},
+    { id: '2', code: 'LAX', city: 'Los Angeles'},
+    { id: '3', code: 'BOS', city: 'Boston'},
+    { id: '4', code: 'MDW', city: 'Chicago'},
+    { id: '5', code: 'PDX', city: 'Portland'},
+    { id: '6', code: 'AUS', city: 'Austin'}
+    ]
 end
 
 
